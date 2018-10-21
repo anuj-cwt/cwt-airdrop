7e35ce7dfc9c5e353076f3cb3a84c6aae910f0c6
Anuj Tiwari <tywaryanuj83@gmail.com>
2018-10-21 14:54:45 +0530

    project upload

---
+++ resources/views/mail/register.blade.php
@@ -0,0 +1,69 @@
+<table width="100%" style="background-color: #f6f6f6;width: 100%; font-family: "Helvetica Neue","Helvetica",Helvetica,Arial,sans-serif">
+<tr>
+    <td></td>
+    <td width="600" style="clear: both;display: block;margin: 0 auto; max-width: 600px; vertical-align: top;">
+        <div style="display: block;
+    margin: 0 auto;
+    max-width: 600px;
+    padding: 20px;">
+            <table  style="background: #fff none repeat scroll 0 0;
+    border: 1px solid #e9e9e9;
+    border-radius: 3px;" width="100%" cellpadding="0" cellspacing="0">
+                <tr>
+                    <td  style="padding: 20px; vertical-align: top;">
+                        <table  cellpadding="0" cellspacing="0" style="width: 550px">
+                            <tr>
+                                <td style="height: 140px; text-align: center; background: #00BA95; color:#fff"><div style="font-size: 25px;font-weight: bold"><img src="https://lalaworld.io/public/skin/images/LaLaWorld-logo-white.png" width="200"></div>
+                                    <div style="font-size: 12px">An Ecosystem for the migrants and their unbanked families</div>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 0 0 20px;vertical-align: top;">
+                                    <h3 style="text-align:center;font-size: 18px;margin: 40px 0 0; font-weight: normal">Welcome to LALA World Community</h3>
+                                </td>
+                            </tr>
+                            <tr>
+                                <td style="padding: 0 0 20px; font-size: 13px; line-height: 1.5; text-align: center;">
+                            <span style="font-size:14px">Thank you for participating in LALA Airdrop and becoming a part of our community.
+                            </span><br/><br/>
+
+                            <span>LALA World is an Ecosystem of real-life partnerships with governments, NGO’s, local communities and an integration of biometrics, blockchain and distributed ledger technology that aims towards financial inclusion.</span>
+                                </td>
+                            </tr>
+
+                            <tr>
+                                <td style="padding: 0 0 20px; font-size: 13px; line-height: 1.5; text-align: center;">
+                            To know more:
+                            <br/>
+                            <a href="https://lalaworld.io">Website</a>
+                            <a href="http://lalaworld.io/whitepaper.pdf">Whitepaper</a>
+                                </td>
+                            </tr>
+
+                            <tr>
+
+                            </tr>
+
+                        </table>
+                    </td>
+                </tr>
+            </table>
+            <div>
+                <table width="590">
+                    <tbody>
+                    <tr>
+                        <td style="background: #F6F6F6 ;text-align: center; color:#000; padding:20px">
+                            <div style="font-size: 14px; font-weight:bold; padding-bottom:10px">Support : hello@lalaworld.io</div>
+                            <span style="font-size:12px">Singapore | India | Malaysia | Dubai | Bangladesh</span>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    </td>
+    <td></td>
+</tr>
+</table>
+
+
